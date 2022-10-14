07e4829f74a196f1cf7243b769aece95aa872d3a
totto2727 <kaihatu.totto2727@gmail.com>
2022-10-15 02:26:57 +0900

    本番用に改造

--- Program.fs
+++ Program.fs
@@ -3,21 +3,23 @@ open System
 open System.Net
 open System.Net.Http
 
-let client = new HttpClient()
-client.DefaultRequestVersion <- HttpVersion.Version30
-client.DefaultVersionPolicy <- HttpVersionPolicy.RequestVersionOrLower
+for i = 1 to 10 do
+  use client = new HttpClient()
+  client.DefaultRequestVersion <- HttpVersion.Version20
+  // client.DefaultRequestVersion <- HttpVersion.Version30
+  client.DefaultVersionPolicy <- HttpVersionPolicy.RequestVersionExact
 
-let req = task {
- return! client.GetAsync("https://tottoquic.ml")
-}
+  let req = task {
+   return! client.GetAsync("https://tottoquic.ml")
+  }
 
-[1 .. 10]
-|> Seq.map (fun n -> task {
-    let! res = req
-    return n, res
-})
-|> Seq.map Async.AwaitTask
-|> Async.Parallel
-|> Async.RunSynchronously
-|> Array.map (fun (n, res) -> printfn($"{n} H:{res.Version} S:{res.StatusCode}"))
-|> ignore
+  [1 .. 10]
+  |> Seq.map (fun n -> task {
+      let! res = req
+      return n, res
+  })
+  |> Seq.map Async.AwaitTask
+  |> Async.Parallel
+  |> Async.RunSynchronously
+  |> Array.map (fun (n, res) -> printfn($"{n} V:{res.Version} S:{res.StatusCode}"))
+  |> ignore

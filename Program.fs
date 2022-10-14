206265fd06e0af5674c3b56055aef83c55a5b6ee
totto2727 <kaihatu.totto2727@gmail.com>
2022-10-14 14:32:34 +0900

    initial commit

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open System.Net
+open System.Net.Http
+
+let client = new HttpClient()
+
+let req = task {
+ return! client.GetStringAsync("https://tottoquic.ml")
+} 
+
+let res = req |> Async.AwaitTask |> Async.RunSynchronously
+
+printfn($"{res}")   

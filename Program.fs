17d6cad4a173f55f9508197d69dd905ca349927d
totto2727 <kaihatu.totto2727@gmail.com>
2022-11-04 18:20:56 +0900

    更新

--- Program.fs
+++ Program.fs
@@ -1,15 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open System
+﻿open System
 open System.Net
 open System.Net.Http
+
+let headers = new Headers.CacheControlHeaderValue()
+headers.NoCache <- true
+headers.NoStore <- true
 
 for i = 1 to 10 do
   use client = new HttpClient()
   client.DefaultRequestVersion <- HttpVersion.Version20
-  // client.DefaultRequestVersion <- HttpVersion.Version30
+  //client.DefaultRequestVersion <- HttpVersion.Version30
   client.DefaultVersionPolicy <- HttpVersionPolicy.RequestVersionExact
   
-  client.DefaultRequestHeaders.CacheControl.NoCache <- true
+  client.DefaultRequestHeaders.CacheControl <- headers
 
   let req = task {
    return! client.GetAsync("https://tottoquic.ml")
--- main.fsx
+++ main.fsx
@@ -1,11 +1,31 @@
-open System.Threading.Tasks
 open System
-[1 .. 10]
-|> Seq.map (fun n -> task { 
-    do! Task.Delay(1000 / n)
-    printfn($"{n}");
-    return n})
-|> Seq.map Async.AwaitTask
-|> Async.Parallel
-|> Async.RunSynchronously
-|> Array.map (fun n -> printfn($"{n}"))
+open System.Net
+open System.Net.Http
+
+let headers = new Headers.CacheControlHeaderValue()
+headers.NoCache <- true
+headers.NoStore <- true
+
+for i = 1 to 10 do
+  use client = new HttpClient()
+  // client.DefaultRequestVersion <- HttpVersion.Version20
+  client.DefaultRequestVersion <- HttpVersion.Version30
+  client.DefaultVersionPolicy <- HttpVersionPolicy.RequestVersionExact
+  
+  client.DefaultRequestHeaders.CacheControl <- headers
+
+  let req = task {
+   return! client.GetAsync("https://tottoquic.ml")
+  }
+
+  [1 .. 10]
+  |> Seq.map (fun n -> task {
+      let! res = req
+      return n, res
+  })
+  |> Seq.map Async.AwaitTask
+  |> Async.Parallel
+  |> Async.RunSynchronously
+  |> Array.map (fun (n, res) -> printfn($"{n} V:{res.Version} S:{res.StatusCode}"))
+  |> ignore
+
